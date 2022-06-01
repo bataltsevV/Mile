3558e5eadf0e942feac8a7c649c93e2576e831af
bataltsevV <mr.podstrelennyy@mail.ru>
2022-06-01 13:37:50 +0300

    Ваши мили

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketsCost = 1000;
+        int mileForOne = 20;
+        System.out.println( " Ваши мили: " + (ticketsCost / mileForOne));
+
+    }
+}
