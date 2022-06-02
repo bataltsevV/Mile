59ee8d15f789b1784131f52365feb63d3126709e
bataltsevV <mr.podstrelennyy@mail.ru>
2022-06-02 10:44:20 +0300

    Modified Main, created BonusMilesServirce

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate (int cost) {
+         int milesCost = 20;
+         int mile = cost / milesCost ;
+         return mile;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticketsCost = 1000;
-        int mileForOne = 20;
-        System.out.println( " Ваши мили: " + (ticketsCost / mileForOne));
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
